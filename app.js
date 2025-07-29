6a18f52127766e280e7890140515bf2281abe9bb
Michael Mina <michael@liquidwebmasters.com>
2025-07-29 03:25:50 +0000

    save spot price with new product prices fix indentation in source files

--- app.js
+++ app.js
@@ -23,11 +23,11 @@ const port = process.env.PORT || 3000;
 // Configure PostgreSQL
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT,
 });
 
 // Middleware
@@ -42,61 +42,62 @@ let lastUpdateTime = null;
 
 // Called whenever /incoming receives a new value
 function scheduleEmit() {
-  updatePending = true;
-  lastUpdateTime = Date.now();
+    updatePending = true;
+    lastUpdateTime = Date.now();
 
-  // Reset debounce timer: emit if quiet for 10s
-  if (debounceTimer) clearTimeout(debounceTimer);
-  debounceTimer = setTimeout(() => {
-    const now = Date.now();
-    const timeSinceLastUpdate = now - lastUpdateTime;
+    // Reset debounce timer: emit if quiet for 10s
+    if (debounceTimer) clearTimeout(debounceTimer);
+    debounceTimer = setTimeout(() => {
+        const now = Date.now();
+        const timeSinceLastUpdate = now - lastUpdateTime;
 
-    if (timeSinceLastUpdate >= 10_000 && updatePending) {
-      emitLatestPrices();
-    }
-  }, 10_000);
+        if (timeSinceLastUpdate >= 10_000 && updatePending) {
+            emitLatestPrices();
+        }
+    }, 10_000);
 }
 
 // Force emit after 30s no matter what
 setInterval(() => {
-  const now = Date.now();
-  const timeSinceLastEmit = now - lastEmitTime;
+    const now = Date.now();
+    const timeSinceLastEmit = now - lastEmitTime;
 
-  if (updatePending && timeSinceLastEmit >= 30_000) {
-    emitLatestPrices();
-  }
+    if (updatePending && timeSinceLastEmit >= 30_000) {
+        emitLatestPrices();
+    }
 }, 5_000);
 
 // Do the actual emit
 function emitLatestPrices() {
-  updatePending = false;
-  lastEmitTime = Date.now();
-  if (debounceTimer) {
-    clearTimeout(debounceTimer);
-    debounceTimer = null;
-  }
+    updatePending = false;
+    lastEmitTime = Date.now();
+    if (debounceTimer) {
+        clearTimeout(debounceTimer);
+        debounceTimer = null;
+    }
 
-  getLatestPrices().then(latestData => {
-    io.emit('priceUpdateBatch', latestData);
-  }).catch(console.error);
+    getLatestPrices().then(latestData => {
+        io.emit('priceUpdateBatch', latestData);
+    }).catch(console.error);
 }
 
 
 
 async function getLatestPrices(productId = null) {
-  const values = [];
-  let whereClause = '';
+    const values = [];
+    let whereClause = '';
 
-  if (productId) {
-    whereClause = 'WHERE product_id = $1';
-    values.push(productId);
-  }
+    if (productId) {
+        whereClause = 'WHERE product_id = $1';
+        values.push(productId);
+    }
 
-  const query = `
+    const query = `
     SELECT DISTINCT ON (product_id, store_id)
       prt.store_id,
       prt.product_id,
       price,
+      spot_price,
       timestamp,
       s.name as store_name,
       s.url as store_url,
@@ -110,76 +111,88 @@ async function getLatestPrices(productId = null) {
     ORDER BY product_id, store_id, timestamp DESC
   `;
 
-  const result = await pool.query(query, values);
+    const result = await pool.query(query, values);
 
-  // separate this out into products
-  const products = result.rows.reduce((acc, item) => {
-    if (!acc[item.product_id]) {
-      acc[item.product_id] = [];
-    }
-    acc[item.product_id].push(item);
-    return acc;
-  }, {});
+    // separate this out into products
+    const products = result.rows.reduce((acc, item) => {
+        if (!acc[item.product_id]) {
+            acc[item.product_id] = [];
+        }
+        acc[item.product_id].push(item);
+        return acc;
+    }, {});
 
-  // Now sort each group by price
-  Object.values(products).forEach(group => {
-    group.sort((a, b) => a.price - b.price); // ascending order
-  });
+    // Now sort each group by price
+    Object.values(products).forEach(group => {
+        group.sort((a, b) => a.price - b.price); // ascending order
+    });
 
-  console.log(products);
-  return products;
+    return products;
 }
 
 
 
 // POST /incoming - Add items to a table
 app.post('/incoming', async (req, res) => {
-  const { c,t,s,p,v } = req.body;
+    const { c, t, s, p, v } = req.body;
 
-  if (!c || !t || !s || !p || v == null) {
-    return res.status(400).json({ error: 'Missing fields' });
-  }
+    if (!c || !t || !s || !p || v == null) {
+        return res.status(400).json({ error: 'Missing fields' });
+    }
 
-  try {
-    await pool.query('INSERT INTO troy_prices_real_time (timestamp, store_id, product_id, source_id, price) VALUES ($1, $2, $3, $4, $5)', [t,s,p,c,v]);
-    console.log('UPDATED: ', [t,s,p,c,v]);
-    updatePending = true;
-    scheduleEmit();
+    try {
+        await pool.query(`
+            INSERT INTO troy_prices_real_time (timestamp, store_id, product_id, source_id, price, spot_price)
+            VALUES ($1, $2, $3, $4, $5,
+            (
+                SELECT ask
+                FROM spot_prices_real_time
+                WHERE symbol = 'XAU/CAD'
+                ORDER BY time DESC
+                LIMIT 1
+            )
+            )`
+            , [t, s, p, c, v]
+        );
 
-    res.json({ message: 'Item added successfully' });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'Database error' });
-  }
+        console.log('UPDATED: ', [t, s, p, c, v]);
+        updatePending = true;
+        scheduleEmit();
+
+        res.json({ message: 'Item added successfully' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Database error' });
+    }
 });
 
 
 // GET /prices - Return all items
 app.get('/prices', async (req, res) => {
-  const { p } = req.query;
+    const { p } = req.query;
 
-  try {
-    const rows = await getLatestPrices(p);
+    try {
+        const rows = await getLatestPrices(p);
 
-    res.json(rows);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'Database error' });
-  }
+        res.json(rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Database error' });
+    }
 });
 
 
 // GET /index - Render prices as HTML
 app.get('/', async (req, res) => {
-  try {
-    const products = await getLatestPrices();
-    res.render('index', {products: products});
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Error loading index');
-  }
+    try {
+        const products = await getLatestPrices();
+        res.render('index', { products: products });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error loading index');
+    }
 });
 
 server.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+    console.log(`Server is running at http://localhost:${port}`);
 });
